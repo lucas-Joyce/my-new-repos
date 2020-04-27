e4d278a2ae87dbd4104a3aa41a90ce38a3a5073b
Lucas Joyce <lucasjoyce1@gmail.com>
2020-04-27 13:48:06 +0000

    understanding api

--- Ajax-Test/main.js
+++ Ajax-Test/main.js
@@ -1,8 +1,19 @@
 var xhr = new XMLHttpRequest();
+var data;
+xhr.open("GET", "philippines-topo2.json");
+//xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+xhr.send();
+
+function setData(jsonData) {
+    data = jsonData;
+    console.log(data);
+}
 xhr.onreadystatechange = function(){
     if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("data").innerHTML = this.responseText;
+        //document.getElementById("data").innerHTML = this.responseText;
+        //console.log(JSON.parse(this.responseText));
+        //data = this.responseText;
+        //
+        setData(JSON.parse(this.responseText));
     }
 };
-xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
-xhr.send();
